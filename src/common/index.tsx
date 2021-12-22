f60f4c39c1757e6f4e561d342e8d3cb8bb4d0c39
Jonatha Gomes <jonatha.dev.js@gmail.com>
2021-12-22 18:51:20 -0300

    rename components to uppercase

--- src/common/index.tsx
+++ src/common/index.tsx
@@ -1,7 +1,7 @@
-import { Input } from "./input";
-import { Cards } from "./cards";
-import { Loading } from "./loading";
-import { Pagination } from "./pagination";
-import { Filter } from "./filter";
+import { Input } from "./Input";
+import { Cards } from "./Cards";
+import { Loading } from "./Loading";
+import { Pagination } from "./Pagination";
+import { Filter } from "./Filter";
 
 export { Input, Cards, Loading, Pagination, Filter };
